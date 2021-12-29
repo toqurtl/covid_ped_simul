d049a264ad7c9578c367ed1927841d24d7ac2792
toqurtl <yoon92411@snu.ac.kr>
2021-12-30 08:01:53 +0900

    simulator

--- app_validate.py
+++ app_validate.py
@@ -13,5 +13,7 @@ origin_path = "data/result/"+idx+"/result_"+idx+".json"
 result_data = ResultData(origin_path, gt_path)
 
 for person_idx in range(0, result_data.num_person):
-    print(result_data.ade(person_idx))
+    print(result_data.ade_of_person(person_idx))
+
+print(result_data.ade_of_scene())
 
